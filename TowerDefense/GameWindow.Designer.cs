9c5325eef9f1d0a20282140fef41e61064315a68
johnathan.davis@outlook.com <johnathan.davis@outlook.com>
2015-03-25 08:12:00 -0500

    Initial Commit. Students will take it from here.

---
+++ TowerDefense/GameWindow.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace TowerDefense
+{
+    partial class GameWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.fpsTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // fpsTimer
+            // 
+            this.fpsTimer.Enabled = true;
+            this.fpsTimer.Interval = 10;
+            this.fpsTimer.Tick += new System.EventHandler(this.fpsTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1331, 970);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Tower Defense";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer fpsTimer;
+    }
+}
+
---
+++ TowerDefense/GameWindow.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace TowerDefense
+{
+    public partial class GameWindow : Form
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawFrame(Graphics screen)
+        {
+            screen.Clear(Color.Black);
+        }
+
+
+        #region "Window Stuff"
+
+        private void fpsTimer_Tick(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Keys.Escape)) this.Close();
+            DrawFrame(e.Graphics);
+        }
+        public abstract class Keyboard
+        {
+            [Flags]
+            private enum KeyStates
+            {
+                None = 0,
+                Down = 1,
+                Toggled = 2
+            }
+
+            [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+            private static extern short GetKeyState(int keyCode);
+
+            private static KeyStates GetKeyState(Keys key)
+            {
+                KeyStates state = KeyStates.None;
+
+                short retVal = GetKeyState((int)key);
+
+                //If the high-order bit is 1, the key is down
+                //otherwise, it is up.
+                if ((retVal & 0x8000) == 0x8000)
+                    state |= KeyStates.Down;
+
+                //If the low-order bit is 1, the key is toggled.
+                if ((retVal & 1) == 1)
+                    state |= KeyStates.Toggled;
+
+                return state;
+            }
+
+            public static bool IsKeyDown(Keys key)
+            {
+                return KeyStates.Down == (GetKeyState(key) & KeyStates.Down);
+            }
+
+            public static bool IsKeyToggled(Keys key)
+            {
+                return KeyStates.Toggled == (GetKeyState(key) & KeyStates.Toggled);
+            }
+        }
+
+        #endregion
+    }
+}
