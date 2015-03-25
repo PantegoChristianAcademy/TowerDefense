89acb5c322e314c65f43330ac2347513f13a95b0
Andrew-Rutledge <andrew.rutledge@pantego.com>
2015-03-25 15:14:04 -0500

    Added a Media folder

---
+++ TowerDefense/EnemyStats.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense
+{
+    class EnemyStats
+    {
+       public short health;
+       public byte LifeLossValue;
+       public short
+    }
+}
