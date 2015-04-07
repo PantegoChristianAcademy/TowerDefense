f052953dd61ad3012e46fb8a075bfa4778a94c04
NoahHasDiabetes <noah.flint@pantego.com>
2015-04-07 14:56:11 -0500

    Added Splash class

---
+++ TowerDefense/Model/Turrets/Base Tower.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense.Model.Turrets
+{
+    public abstract class Base_Tower
+    {
+        public int Damage;
+        // How much damage the turret does
+        public float Firerate;
+        // how many shots per second
+        public int Range;
+        // radius from turret in 
+        public int Cost;
+        // How much gold/money/whatever a turret costs
+        public int TotalCost;
+        //
+        public int SlowPercent;
+        public int DoT;
+        public int BlastRadius;
+        public int Resell
+        {
+            get
+            {
+                return ResellPercentage * Cost;
+            }
+        }
+        public int ResellPercentage;
+        public string additionaleffect;
+        public int upgradelevel = 1;
+
+        public abstract void Upgrade();
+    }
+
+}
---
+++ TowerDefense/Model/Turrets/Splash Tower.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense.Model.Turrets
+{
+    class Splash_Tower : Base_Tower
+    {
+        public Splash_Tower()
+        {
+            Damage = 25;
+            Firerate = 1;
+            Range = 140;
+            Cost = 165;
+            ResellPercentage = 60;
+            additionaleffect = null;
+            BlastRadius = 40;
+        }
+        public override void upgrade()
+        {
+            upgradelevel++;
+            switch(upgradelevel)
+            {
+                case 2:
+                    Damage = 50;
+                    Firerate = 1;
+                    Range = 150;
+                    Cost = 120;
+                    ResellPercentage = 65;
+                    additionaleffect = null;
+                    BlastRadius = 50;
+                    break;
+                case 3:
+                    Damage = 75;
+                    Firerate = 1;
+                    Range = 160;
+                    Cost = 145;
+                    ResellPercentage = 70;
+                    additionaleffect = null;
+                    BlastRadius = 60;
+                    break;
+            }
+            TotalCost = TotalCost + Cost;
+        }
+    }
+ }
