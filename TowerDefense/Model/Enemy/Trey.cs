87231838113e6619f7a879b2340712e8e64d2577
BlakeHobby <blake.hobby@pantego.com>
2015-04-21 14:08:17 -0500

    Added Trey Boss Class

---
+++ TowerDefense/Model/Enemy/Trey.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense.Model.Enemy
+{
+   public class Trey: TowerDefense.Model.Enemies.Enemy
+    {
+       public Trey()
+       {
+           Imagefile = "Media\\Enemies\\Trey.bmp";
+           LoadImage();
+           Health = 230;
+           Speed = 1;
+           Goldgiven = 230;
+           damage = 23;
+       }
+    }
+}
