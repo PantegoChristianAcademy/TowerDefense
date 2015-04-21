0adbe1a71b85cdd8a803f8a4c1f52778cdfd2597
BlakeHobby <blake.hobby@pantego.com>
2015-04-21 14:17:30 -0500

    Added William Boss Class and "balanced" bosses.

---
+++ TowerDefense/Model/Enemy/William.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense.Model.Enemy
+{
+    public class William : TowerDefense.Model.Enemies.Enemy
+    {
+        public William()
+        {
+            Imagefile = "Media\\Enemies\\William.bmp";
+            LoadImage();
+            Health = 420;
+            Speed = 3;
+            Goldgiven = 420;
+            damage = 42;
+        }
+        
+    
+}
